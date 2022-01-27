3822d53131eca946b61052af99feef9b4a6e5e85
engrtunze <tunde4excel@gmail.com>
2022-01-27 08:54:54 +0100

    chapter 4 ended

--- test/isbnclasspractise/ISBNValidatorTest.java
+++ test/isbnclasspractise/ISBNValidatorTest.java
@@ -3,9 +3,9 @@ package isbnclasspractise;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ISBNValidatorTest {
@@ -21,21 +21,51 @@ class ISBNValidatorTest {
     }
 
     @Test
+     void checkAvailable13DigitISBN()
+    {
+        ISBNValidator validator = new ISBNValidator();
+        boolean result = validator.checkISBN("9781853260087");
+        assertTrue(result);
+
+    }
+
+
+    @Test
+    void ISBNNumbersEndingInAnXAreValid()
+    {
+        ISBNValidator validator = new ISBNValidator();
+
+            boolean result = validator.checkISBN("012000030X");
+        assertTrue(result);
+    }
+    @Test
     void checkAnInvalidISBN() {
         ISBNValidator validator = new ISBNValidator();
         boolean result = validator.checkISBN("0140449117");
         assertFalse(result);
     }
 
-    @Test()
+    @Test
     void nineDigitISBNAreNotAllowed()
     {
         ISBNValidator validator = new ISBNValidator();
        var exception = assertThrows(NumberFormatException.class, () -> {
            validator.checkISBN("123456789");
        });
+
         Assertions.assertEquals("ISBN numbers must be 10 digits long", exception.getMessage());
+
     }
 
+    @Test
+    void nonNumericISBNAReNotAllowed()
+    {
+        ISBNValidator validator = new ISBNValidator();
+        var exception = assertThrows(NumberFormatException.class, () -> {
+            validator.checkISBN("helloworld");
+        });
+
+        Assertions.assertEquals("ISBN numbers can only contain numeric digits", exception.getMessage());
+    }
 
 }
